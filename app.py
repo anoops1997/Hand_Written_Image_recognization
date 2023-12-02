c8631e407a8f3dc1756209b674ef8313ee229073
anoops1997 <anoopspillaimgd@gmail.com>
2023-12-02 17:14:27 +0530

    modified:   app.py

--- app.py
+++ app.py
@@ -1,47 +1,35 @@
-from flask import Flask, render_template, request, jsonify
+import streamlit as st
 import numpy as np
 from keras.models import load_model
-from keras.preprocessing import image
-from io import BytesIO
-import base64
+from PIL import Image  # Import Image module from PIL for image processing
 
-app = Flask(__name__)
-
-# Load the pre-trained model
+# Load the trained model
 model = load_model('mnist_model.h5')
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+# Streamlit app
+st.title("MNIST Digit Classification App")
+st.sidebar.header("User Input")
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    try:
-        # Get the image data from the request
-        data_url = request.form['image']
-        header, encoded = data_url.split(",", 1)
-        image_data = BytesIO(base64.b64decode(encoded))
+# Upload image through streamlit sidebar
+uploaded_file = st.sidebar.file_uploader("Choose a digit image...", type="jpg")
 
-        # Read the image
-        img = image.load_img(image_data, target_size=(28, 28), color_mode="grayscale")
-
-        # Convert the image to a numpy array
-        img_array = image.img_to_array(img)
-        img_array = np.expand_dims(img_array, axis=0) / 255.0  
-
-        # Make a prediction
-        prediction = model.predict(img_array)
-
-        # Get the predicted digit
-        predicted_digit = np.argmax(prediction)
-
-        return jsonify({'result': str(predicted_digit)})
+if uploaded_file is not None:
+    # Display the uploaded image
+    st.sidebar.image(uploaded_file, caption="Uploaded Image.", use_column_width=True)
     
-    except Exception as e:
-        print("Error:", str(e))
-        return jsonify({'error': str(e)})
+    # Preprocess the image for the model
+    img = Image.open(uploaded_file)  # Use 'img' instead of 'image'
+    img = img.resize((28, 28))
+    img = np.array(img)
+    img = img.reshape((1, 28, 28, 1)).astype('float32') / 255.0
 
+    # Make prediction
+    prediction = model.predict(img)
+    predicted_class = np.argmax(prediction)
 
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    st.write("")
+    st.write("Classifying...")
+    
+    # Display the predicted digit and confidence
+    st.write(f"Predicted Digit: {predicted_class}")
+    st.write(f"Confidence: {prediction[0][predicted_class]:.2%}")
